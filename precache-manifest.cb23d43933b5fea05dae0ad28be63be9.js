0c73b2cc4ed29dfeced66f560e0e5519b610d3ce
Erica Piano <ericapiano@Ericas-MacBook-Pro.local>
2019-01-08 17:41:41 -0500

    Updates

---
+++ precache-manifest.cb23d43933b5fea05dae0ad28be63be9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reactclickygame/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "961f2cf3bc081edce835",
+    "url": "/reactclickygame/static/js/runtime~main.961f2cf3.js"
+  },
+  {
+    "revision": "de35e51d7d1fee3a5948",
+    "url": "/reactclickygame/static/js/main.de35e51d.chunk.js"
+  },
+  {
+    "revision": "9c49b1657f794805e3dd",
+    "url": "/reactclickygame/static/js/1.9c49b165.chunk.js"
+  },
+  {
+    "revision": "de35e51d7d1fee3a5948",
+    "url": "/reactclickygame/static/css/main.7383d113.chunk.css"
+  },
+  {
+    "revision": "97d9fc97338cb9689d068980b7e5dc00",
+    "url": "/reactclickygame/index.html"
+  }
+];
